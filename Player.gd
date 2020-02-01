f3c9a0f4bfb598e4f4566d718dab60c5da8cc1ac
Alper Sari <alper.sari.work@gmail.com>
2020-02-01 19:59:20 +0300

    Implement Ramp Jump

--- Player.gd
+++ Player.gd
@@ -1,53 +1,78 @@
 extends KinematicBody2D
 
-export (int) var speed = 120
+export (int) var speed = 200
 var moveSpeed
 export (int) var jumpSpeed = -400
-export (int) var bounceSpeed = 250
-export (int) var gravity = 10
+export (int) var bounceSpeed = -400
+export (float) var gravity = 100
 
 var running
-var wallJump
 var wallJumpEnable
 var fallFlag
 
+
 var bounceDir
 var velocity = Vector2()
+var vSpeed = 0
 var airborne = false
+var jumping = false
 
 func _ready():
 	moveSpeed = speed
 	bounceDir = 0
 	running = false
 	fallFlag = false
-	wallJump = true
 	wallJumpEnable = false
 
 func get_input():
-	velocity.x = 0
+	
 	var right = Input.is_action_pressed('ui_right')
 	var left = Input.is_action_pressed('ui_left')
 	var jump = Input.is_action_just_pressed('ui_jump')
 	var bounce = Input.is_action_just_pressed('ui_bounce')
 
+
 	if jump and is_on_floor():
-		velocity.y = jumpSpeed
+		velocity.y += jumpSpeed
+		jumping = true
 	if right:
 		velocity.x += moveSpeed
 	if left:
 		velocity.x -= moveSpeed
 		
 	if bounce && wallJumpEnable:
-		velocity.x += bounceSpeed * bounceDir * 4
-		velocity.y += bounceSpeed * -1
+		#velocity.x += bounceSpeed * bounceDir * 12
+		velocity.y += bounceSpeed
 		wallJumpEnable = false
 	
 		
 
 func _physics_process(delta):
+	
+	
+#	if airborne:
+#		if velocity.x > 0:
+#			velocity.x -= speed / 2
+#		if velocity.x < 0:
+#			velocity.x += speed / 2
+#	else:
+
+		
+	velocity.x = 0
 	get_input()
-	velocity.y += gravity
+	
+	if jumping || !is_on_floor():
+		velocity.y += gravity / 10
+	else:
+		velocity.y += gravity
+		
+	if velocity.y > 0:
+		jumping = false
+	
 	velocity = move_and_slide(velocity, Vector2(0, -1))
+	
+	print(velocity)
+	
 	
 func _process(delta):
 	
@@ -62,13 +87,20 @@ func _process(delta):
 	
 	
 	if !is_on_floor():
+		airborne = true
 		if !fallFlag:
 			$AnimatedSprite.play("Fall Start")
 			fallFlag = true
 		elif $AnimatedSprite.frame > 0:
 			$AnimatedSprite.play("Fall")
+			
+		if right:
+			$AnimatedSprite.flip_h = false
+		if left:
+			$AnimatedSprite.flip_h = true
 		
 	else:
+		airborne = false
 		if $AnimatedSprite.animation == "Fall":
 			$AnimatedSprite.animation = "Run Start"
 		fallFlag = false
@@ -99,10 +131,9 @@ func _process(delta):
 func _on_wallJump_Collider_L_body_entered(body):
 	wallJumpEnable = true
 	bounceDir = 1
-	print("Right")
+	print("Left")
 
 func _on_wallJump_Collider_R_body_entered(body):
 	wallJumpEnable = true
 	bounceDir = -1
-	print("Left")
-
+	print("Right")
