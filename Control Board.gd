f37dee3f761beac2940d3b69a5efc7c4a8792645
Doga Oruc <aeris170@gmail.com>
2020-02-02 13:02:34 +0300

    Make ControlBox emit signal upon repair

--- Control Board.gd
+++ Control Board.gd
@@ -1,5 +1,7 @@
 extends StaticBody2D
 
+signal repair_signal
+
 var isPlayerInRange = false
 
 func _ready():
@@ -8,7 +10,7 @@ func _ready():
 func _process(delta):
 	var repair = Input.is_action_pressed('ui_fire')
 	if isPlayerInRange && repair:
-		print("player repaired!")
+		emit_signal("repair_signal")
 
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("player"):
