5f24e8e0999bc4abadc12777bc0416dc46968856
Alper Sari <alper.sari.work@gmail.com>
2020-02-02 14:46:12 +0300

    Add moving platform

---
+++ Moving Platform.gd
@@ -0,0 +1,35 @@
+extends Node2D
+
+const IDLE = 0.5
+
+export (bool) var active = false
+var moveTo = Vector2.RIGHT * 250
+export var speed = 3.0
+
+onready var platform = $Platform
+onready var tween = $Tween
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	get_parent().get_node("Control Board").connect("repair_signal", self, "on_repaired")
+
+func _process(delta):
+	if active:
+		_init_platform()
+		active = false
+		
+		
+
+func _init_platform():
+	var duration = moveTo.length() / float(speed * 32) # Literal value is tile size
+	tween.interpolate_property(platform, "position", Vector2.ZERO, moveTo, duration, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, IDLE)
+	tween.interpolate_property(platform, "position",  moveTo, Vector2.ZERO, duration, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, duration + IDLE * 2)
+	tween.start()
+
+
+func on_repaired():
+	active = !active
+
+
+func _on_Tween_tween_all_completed():
+	_init_platform()
