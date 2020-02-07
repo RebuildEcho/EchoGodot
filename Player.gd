cfd1907fdb5c5f45f58abcd692b1e3954f276b92
Alper Sari <alper.sari.work@gmail.com>
2020-02-08 01:59:35 +0300

    Remake animation system

--- Player.gd
+++ Player.gd
@@ -6,6 +6,14 @@ export (int) var jumpSpeed = -400
 export (int) var bounceSpeed = -400
 export (float) var gravity = 100
 
+var currAnim
+var prevAnim
+var startAnim
+var finAnim
+var animLock
+#If an animation needs to play for full duration add its name here
+var switchAtEnd = ["Hit"]
+
 var running
 var wallJumpEnable
 var fallFlag
@@ -29,6 +37,12 @@ var dead
 var soundPlayer
 
 func _ready():
+	$AnimationPlayer.play("Idle")
+	currAnim = "Idle"
+	prevAnim = "Idle"
+	startAnim = false
+	finAnim = false
+	animLock = false
 	moveSpeed = speed
 	bounceDir = 0
 	running = false
@@ -98,97 +112,66 @@ func _physics_process(delta):
 	velocity = move_and_slide(velocity, Vector2(0, -1))
 	
 func _process(delta):
-	var right = Input.is_action_pressed('ui_right')
-	var left = Input.is_action_pressed('ui_left')
-	var jump = Input.is_action_just_pressed('ui_jump')
-	$AnimatedSprite.speed_scale = 1.5
-	if airborne:
-		moveSpeed = speed 
-	else:
-		moveSpeed = speed
+#	var right = Input.is_action_just_pressed('ui_right')
+#	var left = Input.is_action_just_pressed('ui_left')
+	var jump = Input.is_action_pressed('ui_jump')
+	var interact = Input.is_action_pressed('ui_fire')
 	
 	if Input.is_key_pressed(KEY_K):
 		dead = true
-		
+	
+	#Add animation control statements here
+	
 	if !dead:
-		if interacting:
-			$AnimatedSprite.speed_scale = 3
-			$AnimatedSprite.play("Attack")
-			if $AnimatedSprite.frame == 7:
-				interacting = false
-		elif !is_on_floor():
-			airborne = true
-			soundPlayer.stop()
-			
-			if !fallFlag:
-				$AnimatedSprite.play("Fall Start")
-				fallFlag = true
-			elif $AnimatedSprite.frame > 0:
-				$AnimatedSprite.play("Fall")
-				
-			if right:
-				$AnimatedSprite.flip_h = false
-			if left:
-				$AnimatedSprite.flip_h = true
-			
-		else:
-			if !inLocked:
-				airborne = false
-				if $AnimatedSprite.animation == "Fall":
-					$AnimatedSprite.animation = "Run Start"
-				fallFlag = false
-				
-				if soundPlayer.get_playback_position() >= 0.3:
-					soundPlayer.stop()
-					pitchFlag = !pitchFlag;
-
-				if right:
-					if !soundPlayer.is_playing():
-						if !pitchFlag:
-							soundPlayer.set_pitch_scale(1.5)
-						else:
-							soundPlayer.set_pitch_scale(1)
-						soundPlayer.play(0)
-					$AnimatedSprite.flip_h = false
-					if !running:
-						$AnimatedSprite.speed_scale = 0.2
-						$AnimatedSprite.play("Run Start")
-						running = true
-					elif $AnimatedSprite.frame == 1:
-						$AnimatedSprite.play("Run")
+		if is_on_floor():
+			if !animLock:
+				if velocity.x > 0 && currAnim != "Run":
+					$AnimationSprites.scale.x = 1
+					currAnim = "Run"
+				if velocity.x < 0 && currAnim != "Run":
+					$AnimationSprites.scale.x = -1
+					currAnim = "Run"		
 					
-				if left:
-					if !soundPlayer.is_playing():
-						if !pitchFlag:
-							soundPlayer.set_pitch_scale(1.5)
-						else:
-							soundPlayer.set_pitch_scale(1)
-						soundPlayer.play(0)
-					$AnimatedSprite.flip_h = true
-					if !running:
-						$AnimatedSprite.speed_scale = 0.2
-						$AnimatedSprite.play("Run Start")
-						running = true
-					elif $AnimatedSprite.frame == 1:
-						$AnimatedSprite.play("Run")
-			if !using:
-				if velocity == Vector2():
-					soundPlayer.stop()
-					$AnimatedSprite.speed_scale = 0.25
-					$AnimatedSprite.play("Idle")
-					running = false
+				if velocity == Vector2(0,0):
+					currAnim = "Idle"
+					
+				if interact:
+					currAnim = "Hit"
+				if jump:
+					currAnim = "Jump"
+		else:
+			currAnim = "Airborne"
+			if prevAnim == "Airborne" && is_on_floor():
+				currAnim = "Land"
 	else:
-		$AnimatedSprite.play("Death")
-		if $AnimatedSprite.animation == "Death":
-			inLocked = true
-			if !dying && $AnimatedSprite.frame == 7:
-				$"Death Timer".start(1)
-				dying = true
-				
-				
+		inLocked = true
+		currAnim = "Death"
+		if $"Death Timer".time_left == 0:
+			$"Death Timer".start(1)
 			
+	switch_animation()
+	
+func switch_animation():
 		
-
+	if !animLock:
+		if prevAnim != currAnim:
+			finAnim = true
+			startAnim = true
+		if finAnim:
+			$AnimationPlayer.stop()
+			finAnim = false
+		if startAnim:
+			$AnimationPlayer.play(currAnim)
+			$AnimationSprites.get_node(prevAnim).hide()
+			$AnimationSprites.get_node(currAnim).show()
+			startAnim = false
+		if prevAnim != currAnim:
+			prevAnim = currAnim
+		if switchAtEnd.has(currAnim):
+			animLock = true	
+		if animLock:
+			$"Lock Timer".start($AnimationPlayer.current_animation_length)
+		
 func _on_wallJump_Collider_L_body_entered(body):
 	wallJumpEnable = true
 	bounceDir = 1
@@ -197,7 +180,6 @@ func _on_wallJump_Collider_R_body_entered(body):
 	wallJumpEnable = true
 	bounceDir = -1
 
-
 func _on_Death_Hitbox_body_entered(body):
 	if body.is_in_group("enemies"):
 		dead = true
@@ -205,3 +187,90 @@ func _on_Death_Hitbox_body_entered(body):
 
 func _on_Timer_timeout():
 	get_tree().reload_current_scene()
+
+func _on_Lock_Timer_timeout():
+	animLock = false
+
+#	if !dead:
+#		if interacting:
+#			$AnimatedSprite.speed_scale = 3
+#			$AnimatedSprite.play("Attack")
+#			if $AnimatedSprite.frame == 7:
+#				interacting = false
+#		elif !is_on_floor():
+#			airborne = true
+#			soundPlayer.stop()
+#
+#			if !fallFlag:
+#				$AnimatedSprite.play("Fall Start")
+#				fallFlag = true
+#			elif $AnimatedSprite.frame > 0:
+#				$AnimatedSprite.play("Fall")
+#
+#			if right:
+#				$AnimatedSprite.flip_h = false
+#			if left:
+#				$AnimatedSprite.flip_h = true
+#
+#		else:
+#			if !inLocked:
+#				airborne = false
+#				if $AnimatedSprite.animation == "Fall":
+#					$AnimatedSprite.animation = "Run Start"
+#				fallFlag = false
+#
+#				if soundPlayer.get_playback_position() >= 0.3:
+#					soundPlayer.stop()
+#					pitchFlag = !pitchFlag;
+#
+#				if right:
+#					if !soundPlayer.is_playing():
+#						if !pitchFlag:
+#							soundPlayer.set_pitch_scale(1.5)
+#						else:
+#							soundPlayer.set_pitch_scale(1)
+#						soundPlayer.play(0)
+#					$AnimatedSprite.flip_h = false
+#					if !running:
+#						$AnimatedSprite.speed_scale = 0.2
+#						$AnimatedSprite.play("Run Start")
+#						running = true
+#					elif $AnimatedSprite.frame == 1:
+#						$AnimatedSprite.play("Run")
+#
+#				if left:
+#					if !soundPlayer.is_playing():
+#						if !pitchFlag:
+#							soundPlayer.set_pitch_scale(1.5)
+#						else:
+#							soundPlayer.set_pitch_scale(1)
+#						soundPlayer.play(0)
+#					$AnimatedSprite.flip_h = true
+#					if !running:
+#						$AnimatedSprite.speed_scale = 0.2
+#						$AnimatedSprite.play("Run Start")
+#						running = true
+#					elif $AnimatedSprite.frame == 1:
+#						$AnimatedSprite.play("Run")
+#			if !using:
+#				if velocity == Vector2():
+#					soundPlayer.stop()
+#					$AnimatedSprite.speed_scale = 0.25
+#					$AnimatedSprite.play("Idle")
+#					running = false
+#	else:
+#		$AnimatedSprite.play("Death")
+#		if $AnimatedSprite.animation == "Death":
+#			inLocked = true
+#			if !dying && $AnimatedSprite.frame == 7:
+#				$"Death Timer".start(1)
+#				dying = true
+#
+				
+			
+
+
+
+
+
+
